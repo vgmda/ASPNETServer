4d1a44ba0511c0b7d1ab89146a6e134cc47be67b
vgmda <vasilegancin@gmail.com>
2022-08-16 23:43:11 +0100

    Data Migration

--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace ASPNETServer.Data;
 
-public class AppDBContext
+internal sealed class AppDBContext : DbContext
 {
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) =>
+        dbContextOptionsBuilder.UseSqlite("Data Source=./Data/AppDB.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        Post[] postsToSeed = new Post[6];
+
+        for (int i = 1; i <= 6; i++)
+        {
+            postsToSeed[i - 1] = new Post
+            {
+                PostId = i,
+                Title = $"Post {i}",
+                Content = $"This is post {i} and it has some content."
+            };
+
+        }
+        modelBuilder.Entity<Post>().HasData(postsToSeed);
+    }
 
 }
 
