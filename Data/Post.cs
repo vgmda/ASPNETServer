99afd2a560837983d7acb512d00cc9edd4a56272
vgmda <vasilegancin@gmail.com>
2022-08-16 23:29:10 +0100

    DB implementation

---
+++ Data/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPNETServer.Data;
+
+
+// Internal - only available in this project/solution
+// Sealed - It can't be inherited from
+
+internal sealed class Post
+{
+    // Data Annotations
+    [Key]
+    public int PostId { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100000)]
+    public string Content { get; set; } = string.Empty;
+
+
+
+}
+
