a00f3ef5902746c0acaaf83d9820d015c14f4ada
vgmda <vasilegancin@gmail.com>
2022-08-18 00:13:44 +0100

    App running, refactoring code

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
 ﻿using ASPNETServer.Data;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "ASP.NET React", Version = "v1" });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(swaggerUIOptions =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    swaggerUIOptions.DocumentTitle = "ASP.NET React";
+    swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API with POST model.");
+    swaggerUIOptions.RoutePrefix = String.Empty;
+
+});
 
 app.UseHttpsRedirection();
 
-app.MapGet("/get-all-posts", async () => await PostRepository.GetPostsAsync());
+app.MapGet("/get-all-posts", async () => await PostRepository.GetPostsAsync())
+    .WithTags("Posts Endpoints");
+
 
 app.Run();
