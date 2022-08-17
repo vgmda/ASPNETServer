c9c125a0a168cdae1e490aee1d1979623a38d8cb
vgmda <vasilegancin@gmail.com>
2022-08-17 23:27:06 +0100

    Create PostRepository() and its methods

---
+++ Data/PostRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETServer.Data;
+
+internal static class PostRepository
+{
+    internal async static Task<List<Post>> GetPostsAsync()
+    {
+        using (var db = new AppDBContext())
+        {
+            return await db.Posts.ToListAsync();
+        }
+    }
+
+
+    internal async static Task<Post> GetPostByIdAsync(int postId)
+    {
+        using (var db = new AppDBContext())
+        {
+            return await db.Posts.FirstOrDefaultAsync(post => post.PostId == postId);
+        }
+    }
+
+    internal async static Task<bool> CreatePostAsync(Post postToCreate)
+    {
+        using (var db = new AppDBContext())
+        {
+            try
+            {
+                await db.Posts.AddAsync(postToCreate);
+
+                return await db.SaveChangesAsync() >= 1;
+            }
+            catch (Exception e)
+            {
+
+                return false;
+            }
+        }
+    }
+}
+
