506ecbce3d206ed0d5a2be05edb7ef3ccd2f474f
vgmda <vasilegancin@gmail.com>
2022-08-18 20:16:31 +0100

    Add Bootstrap to React app

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -4,9 +4,32 @@ import React from "react";
 
 export default function App() {
   return (
-    <div>
-      Hello world!
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>Hello Bootstrap!</h1>
+        </div>
+      </div>
+
     </div>
   );
+
+
+  function renderPostsTable() {
+    return (
+      <div className="table-responsive mt-5">
+        <table className="table table-bordered border-dark">
+          <thead>
+            <tr>
+              <th scope="col">PostId (PK)</th>
+              <th scope="col">Title</th>
+              <th scope="col">Content</th>
+              <th scope="col">CRUD Operations</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    )
+  }
 }
 
