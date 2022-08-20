3562bd52aeeb415171bd153284c0fce8a853392c
vgmda <vasilegancin@gmail.com>
2022-08-20 20:38:06 +0100

    Create New Post page completed

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import Constants from "./Utilities/Constants";
+import PostCreateForm from "./Components/PostCreateForm";
 
 
 
 export default function App() {
   const [posts, setPosts] = useState([]);
+  const [showingCreateNewPostForm, setShowingCreateNewPostForm] = useState(false);
+
   function getPosts() {
     const url = Constants.API_URL_GET_ALL_POSTS;
 
@@ -25,15 +28,18 @@ export default function App() {
     <div className="container">
       <div className="row min-vh-100">
         <div className="col d-flex flex-column justify-content-center align-items-center">
-          <div>
-            <h1 className="m-5">ASP.NET Core React</h1>
-            <div className="mt-5">
-              <button onClick={getPosts} className='btn btn-dark btn-lg w-100'>Get Posts From Server</button>
-              <button onClick={() => { }} className='btn btn-secondary btn-lg w-100 mt-4'>Create New Post</button>
+          {showingCreateNewPostForm === false && (
+            <div>
+              <h1 className="m-5">ASP.NET Core React</h1>
+              <div className="mt-5">
+                <button onClick={getPosts} className='btn btn-dark btn-lg w-100'>Get Posts From Server</button>
+                <button onClick={() => setShowingCreateNewPostForm(true)} className='btn btn-secondary btn-lg w-100 mt-4'>Create New Post</button>
+              </div>
             </div>
-          </div>
+          )}
 
-          {posts.length > 0 && renderPostsTable()}
+          {(posts.length > 0 && showingCreateNewPostForm === false) && renderPostsTable()}
+          {showingCreateNewPostForm && <PostCreateForm onPostCreated={onPostCreated} />}
         </div>
       </div>
 
@@ -70,6 +76,18 @@ export default function App() {
         <button onClick={() => setPosts([])} className='btn btn-dark btn-lg w-100 my-2'>Empty Posts Array</button>
       </div>
     )
+  }
+
+  function onPostCreated(createdPost) {
+    setShowingCreateNewPostForm(false)
+
+    if (createdPost === null) {
+      return;
+    }
+
+    alert(`Post successfully create. After clicking OK, your new post named "${createdPost.title}" will show up in the table below.`);
+
+    getPosts();
   }
 }
 
--- reactclient/src/Components/PostCreateForm.js
+++ reactclient/src/Components/PostCreateForm.js
@@ -2,15 +2,17 @@ import React, { useState } from 'react'
 import Constants from '../Utilities/Constants'
 
 
-export default function PostCreateForm() {
-
-    const [formData, setFormData] = useState(initialFormData);
+export default function PostCreateForm(props) {
 
     const initialFormData = Object.freeze({
         title: "Post X",
         content: "Test content for Post X",
 
     });
+
+    const [formData, setFormData] = useState(initialFormData);
+
+
 
     const handleChange = (e) => {
         setFormData({
@@ -34,40 +36,40 @@ export default function PostCreateForm() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify{postToCreate};
+            body: JSON.stringify(postToCreate)
         })
             .then(response => response.json())
-            .then(postsFromServer => {
-                setPosts(postsFromServer);
+            .then(responseFromServer => {
+                console.log(responseFromServer);
             })
             .catch((error) => {
                 console.log(error);
                 alert(error);
             });
+
+        props.onPostCreated(postToCreate);
     };
 
 
     return (
-        <div>
-            <form className="w-100 px-5">
-                <h1 className="mt-5">Create New Post</h1>
+        <form className="w-100 px-5">
+            <h1 className="mt-5">Create New Post</h1>
 
-                <div className='mt-5'>
-                    <label className='h3 form-label'>Post Title</label>
-                    <input value={formData.title} name="title" type="text" className='form-control'
-                        onChange={handleChange} />
-                </div>
+            <div className='mt-5'>
+                <label className='h3 form-label'>Post Title</label>
+                <input value={formData.title} name="title" type="text" className='form-control'
+                    onChange={handleChange} />
+            </div>
 
-                <div className='mt-4'>
-                    <label className='h3 form-label'>Post Content</label>
-                    <input value={formData.content} name="content" type="text" className='form-control'
-                        onChange={handleChange} />
-                </div>
+            <div className='mt-4'>
+                <label className='h3 form-label'>Post Content</label>
+                <input value={formData.content} name="content" type="text" className='form-control'
+                    onChange={handleChange} />
+            </div>
 
-                <button onClick={handleSubmit} className="btn btn-dark btn-lg w-100 mt-5">Submit</button>
-                <button onClick={() => props.onPostCreate(null)} className="btn btn-secondary btn-lg w-100 mt-3">Cancel</button>
-            </form>
-        </div>
+            <button onClick={handleSubmit} className="btn btn-dark btn-lg w-100 mt-5">Submit</button>
+            <button onClick={() => props.onPostCreate(null)} className="btn btn-secondary btn-lg w-100 mt-3">Cancel</button>
+        </form>
     );
 }
 
